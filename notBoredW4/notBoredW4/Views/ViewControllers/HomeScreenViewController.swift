c54874221a8c668d06e04ceac5993a21d77dd8e7
Mauro Emmanuel Alvarenga <mauro.alvarenga@mercadolibre.com>
2022-03-24 13:05:54 -0300

    Created new HomeScreen, renamed old one to Test

---
+++ notBoredW4/notBoredW4/Views/ViewControllers/HomeScreenViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  notBoredW4
+//
+//  Created by Mauro Emmanuel Alvarenga on 24/03/2022.
+//
+
+import UIKit
+
+class HomeScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    
+    
+}
